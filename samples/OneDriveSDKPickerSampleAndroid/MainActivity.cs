c6bffb630cefecc9f23936b8f3ee89df56b70d24
Matthew Leibowitz <mattleibow@live.com>
2015-03-13 00:10:38 +0200

    Added the OneDrive SDK Picker and Saver  - binding  - sample for both

--- samples/OneDriveSDKPickerSampleAndroid/MainActivity.cs
+++ samples/OneDriveSDKPickerSampleAndroid/MainActivity.cs
@@ -1,244 +1,165 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 
 using Android.App;
 using Android.Content;
-using Android.Runtime;
+using Android.Graphics;
+using Android.OS;
 using Android.Views;
 using Android.Widget;
-using Android.OS;
 
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+using Microsoft.OneDrive.Pickers;
 
-namespace OneDriveSDKSampleAndroid
+[assembly: UsesPermission (Android.Manifest.Permission.Internet)]
+
+namespace OneDriveSDKPickerSampleAndroid
 {
 	/// <summary>
 	/// Activity that shows how the OneDrive SDK can be used for file picking
 	/// </summary>
-	[Activity (Label = "OneDriveSDKSampleAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/ic_launcher")]
 	public class MainActivity : Activity
 	{
-
 		/// <summary>
 		/// Registered Application id for OneDrive {@link http
 		/// ://go.microsoft.com/fwlink/p/?LinkId=193157}
 		/// </summary>
-		private const string ONEDRIVE_APP_ID = "48122D4E";
-
-		/// <summary>
-		/// The onClickListener that will start the OneDrive Picker
-		/// </summary>
-		private readonly OnClickListener mStartPickingListener = new OnClickListenerAnonymousInnerClassHelper();
-
-		private class OnClickListenerAnonymousInnerClassHelper : OnClickListener
-		{
-			public OnClickListenerAnonymousInnerClassHelper()
-			{
-			}
-
-			public override void OnClick(View v)
-			{
-				// Clear out any previous results
-				outerInstance.ClearResultTable();
-
-				// Determine the link type that was selected
-				LinkType linkType;
-				if (((RadioButton)findViewById(R.id.radioWebViewLink)).Checked)
-				{
-					linkType = LinkType.WebViewLink;
-				}
-				else if (((RadioButton)findViewById(R.id.radioDownloadLink)).Checked)
-				{
-					linkType = LinkType.DownloadLink;
-				}
-				else
-				{
-					throw new Exception("Invalid Radio Button Choosen.");
-				}
-
-				// Start the picker
-				outerInstance.mPicker.startPicking((Activity)v.Context, linkType);
-			}
-		}
-
-		/// <summary>
-		/// Saves the picked file from OneDrive to the device
-		/// </summary>
-		private readonly OnClickListener mSaveLocally = new OnClickListenerAnonymousInnerClassHelper2();
-
-		private class OnClickListenerAnonymousInnerClassHelper2 : OnClickListener
-		{
-			public OnClickListenerAnonymousInnerClassHelper2()
-			{
-			}
-
-			public override void OnClick(View v)
-			{
-				if (outerInstance.mDownloadUrl == null)
-				{
-					return;
-				}
-
-				DownloadManager downloadManager = (DownloadManager)v.Context.getSystemService(DOWNLOAD_SERVICE);
-				Request request = new Request(outerInstance.mDownloadUrl);
-				request.NotificationVisibility = Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED;
-				downloadManager.enqueue(request);
-			}
-		}
+		private const string OneDriveAppId = "48122D4E";
 
 		/// <summary>
 		/// The OneDrive picker instance used by this activity
 		/// </summary>
-		private IPicker mPicker;
+		private IPicker picker;
 
 		/// <summary>
 		/// The picked file's download URI
 		/// </summary>
-		private Uri mDownloadUrl;
+		private Android.Net.Uri downloadUrl;
 
-		protected internal override void OnCreate(Bundle savedInstanceState)
+		protected override void OnCreate (Bundle savedInstanceState)
 		{
-			base.OnCreate(savedInstanceState);
-			ContentView = R.layout.activity_picker_main;
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.MainLayout);
 
 			// Create the picker instance
-			mPicker = Picker.createPicker(ONEDRIVE_APP_ID);
+			picker = Picker.CreatePicker (OneDriveAppId);
 
 			// Add the start picker listener
-			((Button)findViewById(R.id.startPickerButton)).OnClickListener = mStartPickingListener;
+			FindViewById<Button> (Resource.Id.startPickerButton).Click += delegate {
+				// Clear out any previous results
+				ClearResultTable ();
+
+				// Determine the link type that was selected
+				LinkType linkType;
+				if (FindViewById<RadioButton> (Resource.Id.radioWebViewLink).Checked) {
+					linkType = LinkType.WebViewLink;
+				} else if (FindViewById<RadioButton> (Resource.Id.radioDownloadLink).Checked) {
+					linkType = LinkType.DownloadLink;
+				} else {
+					throw new Exception ("Invalid Radio Button Choosen.");
+				}
+
+				// Start the picker
+				picker.StartPicking (this, linkType);
+			};
 
 			// Add the save as listener for download links
-			((Button)findViewById(R.id.saveAsButton)).OnClickListener = mSaveLocally;
+			FindViewById<Button> (Resource.Id.saveAsButton).Click += delegate {
+				if (downloadUrl != null) {
+					var downloadManager = DownloadManager.FromContext (this);
+					var request = new DownloadManager.Request (downloadUrl);
+					request.SetNotificationVisibility (DownloadVisibility.VisibleNotifyCompleted);
+					downloadManager.Enqueue (request);
+				}
+			};
 		}
 
-		protected internal override void OnActivityResult(int requestCode, int resultCode, Intent data)
+		protected override void OnActivityResult (int requestCode, Result resultCode, Intent data)
 		{
 			// Get the results from the from the picker
 			//JAVA TO C# CONVERTER WARNING: The original Java variable was marked 'final':
 			//ORIGINAL LINE: final IPickerResult result = mPicker.getPickerResult(requestCode, resultCode, data);
-			IPickerResult result = mPicker.getPickerResult(requestCode, resultCode, data);
+			var result = picker.GetPickerResult (requestCode, resultCode, data);
 
 			// Handle the case if nothing was picked
-			if (result == null)
-			{
-				Toast.makeText(this, "Did not get a file from the picker!", Toast.LENGTH_LONG).show();
+			if (result != null) {
+				// Update the UI with the picker results
+				UpdateResultTable (result);
+			} else {
+				Toast.MakeText (this, "Did not get a file from the picker!", ToastLength.Long).Show ();
 				return;
 			}
-
-			// Update the UI with the picker results
-			UpdateResultTable(result);
 		}
 
 		/// <summary>
 		/// Updates the results table with details from an <seealso cref="IPickerResult"/>
 		/// </summary>
 		/// <param name="result"> The results of the picker </param>
-		private void UpdateResultTable(IPickerResult result)
+		private void UpdateResultTable (IPickerResult result)
 		{
-			((TextView)findViewById(R.id.nameResult)).Text = result.Name;
-			((TextView)findViewById(R.id.linkTypeResult)).Text = result.LinkType + "";
-			((TextView)findViewById(R.id.linkResult)).Text = result.Link + "";
-			((TextView)findViewById(R.id.fileSizeResult)).Text = result.Size + "";
+			FindViewById<TextView> (Resource.Id.nameResult).Text = result.Name;
+			FindViewById<TextView> (Resource.Id.linkTypeResult).Text = result.LinkType.ToString ();
+			FindViewById<TextView> (Resource.Id.linkResult).Text = result.Link.ToString ();
+			FindViewById<TextView> (Resource.Id.fileSizeResult).Text = result.Size.ToString ();
 
-			Uri thumbnailSmall = result.ThumbnailLinks.get("small");
-			CreateUpdateThumbnail((ImageView)findViewById(R.id.thumbnail_small), thumbnailSmall).execute((Void)null);
-			((TextView)findViewById(R.id.thumbnail_small_uri)).Text = thumbnailSmall + "";
+			var thumbnailSmall = result.ThumbnailLinks ["small"].ToString ();
+			FindViewById<TextView> (Resource.Id.thumbnail_small_uri).Text = thumbnailSmall;
+			CreateUpdateThumbnail (FindViewById<ImageView> (Resource.Id.thumbnail_small), thumbnailSmall);
 
-			Uri thumbnailMedium = result.ThumbnailLinks.get("medium");
-			CreateUpdateThumbnail((ImageView)findViewById(R.id.thumbnail_medium), thumbnailMedium).execute((Void)null);
-			((TextView)findViewById(R.id.thumbnail_medium_uri)).Text = thumbnailMedium + "";
+			var thumbnailMedium = result.ThumbnailLinks ["medium"].ToString ();
+			FindViewById<TextView> (Resource.Id.thumbnail_medium_uri).Text = thumbnailMedium;
+			CreateUpdateThumbnail (FindViewById<ImageView> (Resource.Id.thumbnail_medium), thumbnailMedium);
 
-			Uri thumbnailLarge = result.ThumbnailLinks.get("large");
-			CreateUpdateThumbnail((ImageView)findViewById(R.id.thumbnail_large), thumbnailLarge).execute((Void)null);
-			((TextView)findViewById(R.id.thumbnail_large_uri)).Text = thumbnailLarge + "";
+			var thumbnailLarge = result.ThumbnailLinks ["large"].ToString ();
+			FindViewById<TextView> (Resource.Id.thumbnail_large_uri).Text = thumbnailLarge;
+			CreateUpdateThumbnail (FindViewById<ImageView> (Resource.Id.thumbnail_large), thumbnailLarge);
 
-			findViewById(R.id.thumbnails).Visibility = View.VISIBLE;
+			FindViewById (Resource.Id.thumbnails).Visibility = ViewStates.Visible;
 
-			if (result.LinkType == LinkType.DownloadLink)
-			{
-				findViewById(R.id.saveAsArea).Visibility = View.VISIBLE;
-				mDownloadUrl = result.Link;
+			if (result.LinkType == LinkType.DownloadLink) {
+				FindViewById (Resource.Id.saveAsArea).Visibility = ViewStates.Visible;
+				downloadUrl = result.Link;
 			}
 		}
 
 		/// <summary>
 		/// Clears out all picker results
 		/// </summary>
-		private void ClearResultTable()
+		private void ClearResultTable ()
 		{
-			((TextView)findViewById(R.id.nameResult)).Text = "";
-			((TextView)findViewById(R.id.linkTypeResult)).Text = "";
-			((TextView)findViewById(R.id.linkResult)).Text = "";
-			((TextView)findViewById(R.id.fileSizeResult)).Text = "";
-			findViewById(R.id.thumbnails).Visibility = View.INVISIBLE;
-			findViewById(R.id.saveAsArea).Visibility = View.INVISIBLE;
-			((ImageView)findViewById(R.id.thumbnail_small)).ImageBitmap = null;
-			((TextView)findViewById(R.id.thumbnail_small_uri)).Text = "";
-			((ImageView)findViewById(R.id.thumbnail_medium)).ImageBitmap = null;
-			((TextView)findViewById(R.id.thumbnail_medium_uri)).Text = "";
-			((ImageView)findViewById(R.id.thumbnail_large)).ImageBitmap = null;
-			((TextView)findViewById(R.id.thumbnail_large_uri)).Text = "";
-			mDownloadUrl = null;
+			FindViewById<TextView> (Resource.Id.nameResult).Text = "";
+			FindViewById<TextView> (Resource.Id.linkTypeResult).Text = "";
+			FindViewById<TextView> (Resource.Id.linkResult).Text = "";
+			FindViewById<TextView> (Resource.Id.fileSizeResult).Text = "";
+			FindViewById (Resource.Id.thumbnails).Visibility = ViewStates.Invisible;
+			FindViewById (Resource.Id.saveAsArea).Visibility = ViewStates.Invisible;
+			FindViewById<ImageView> (Resource.Id.thumbnail_small).SetImageBitmap (null);
+			FindViewById<TextView> (Resource.Id.thumbnail_small_uri).Text = "";
+			FindViewById<ImageView> (Resource.Id.thumbnail_medium).SetImageBitmap (null);
+			FindViewById<TextView> (Resource.Id.thumbnail_medium_uri).Text = "";
+			FindViewById<ImageView> (Resource.Id.thumbnail_large).SetImageBitmap (null);
+			FindViewById<TextView> (Resource.Id.thumbnail_large_uri).Text = "";
+			downloadUrl = null;
 		}
 
 		/// <summary>
 		/// Download the thumbnails for display
 		/// </summary>
-		/// <param name="uri"> The uri of the bitmap to retrieve from OneDrive </param>
-		/// <returns> The image as a bitmap </returns>
-		private Bitmap GetBitmap(Uri uri)
-		{
-			try
-			{
-				if (uri == null)
-				{
-					return null;
-				}
-
-				URL url = new URL(uri.ToString());
-				System.IO.Stream inputStream = url.openConnection().InputStream;
-				Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-				inputStream.Close();
-				return bitmap;
-			}
-			catch (ception)
-			{
-				return null;
-			}
-		}
-
-		/// <summary>
-		/// Creates a task that will update a thumbnail
-		/// </summary>
 		/// <param name="imageView"> The image view that should be updated </param>
 		/// <param name="imageSource"> The uri of the image that should be put on the image view </param>
-		/// <returns> The task that will perform this update </returns>
-		private AsyncTask<Void, Void, Bitmap> CreateUpdateThumbnail(ImageView imageView, Uri imageSource)
+		private async Task CreateUpdateThumbnail (ImageView imageView, string imageSource)
 		{
-			return new AsyncTaskAnonymousInnerClassHelper(this, imageView, imageSource);
-		}
-
-		private class AsyncTaskAnonymousInnerClassHelper : AsyncTask<Void, Void, Bitmap>
-		{
-			private readonly PickerMain outerInstance;
-
-			private ImageView imageView;
-			private Uri imageSource;
-
-			public AsyncTaskAnonymousInnerClassHelper(PickerMain outerInstance, ImageView imageView, Uri imageSource)
-			{
-				this.outerInstance = outerInstance;
-				this.imageView = imageView;
-				this.imageSource = imageSource;
-			}
-
-			protected internal override Bitmap DoInBackground(params Void[] @params)
-			{
-				return outerInstance.GetBitmap(imageSource);
-			}
-
-			protected internal override void OnPostExecute(Bitmap result)
-			{
-				imageView.ImageBitmap = result;
+			if (imageSource != null) {
+				try {
+					var result = await WebRequest.CreateDefault (new Uri (imageSource)).GetResponseAsync ();
+					using (var inputStream = result.GetResponseStream ())
+					using (var bitmap = await BitmapFactory.DecodeStreamAsync (inputStream)) {
+						imageView.SetImageBitmap (bitmap);
+					}
+				} catch (Exception ex) {
+					Toast.MakeText (this, "Download Error: " + ex.Message, ToastLength.Long).Show ();
+				}
 			}
 		}
 	}
--- samples/OneDriveSDKSaverSampleAndroid/MainActivity.cs
+++ samples/OneDriveSDKSaverSampleAndroid/MainActivity.cs
@@ -2,34 +2,114 @@
 
 using Android.App;
 using Android.Content;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace OneDriveSDKSampleAndroid
+using Microsoft.OneDrive.Savers;
+using System.IO;
+using System.Threading.Tasks;
+
+[assembly: UsesPermission (Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission (Android.Manifest.Permission.WriteExternalStorage)]
+
+namespace OneDriveSDKSaverSampleAndroid
 {
-	[Activity (Label = "OneDriveSDKSampleAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/ic_launcher")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		/// <summary>
+		/// The default file size
+		/// </summary>
+		private const int DefaultFileSizeKb = 100;
 
-		protected override void OnCreate (Bundle bundle)
+		/// Registered Application id for OneDrive {<seealso cref="http://go.microsoft.com/fwlink/p/?LinkId=193157" />}.
+		private const string OneDriveAppId = "48122D4E";
+
+		/// <summary>
+		/// The OneDrive saver instance used by this activity
+		/// </summary>
+		private ISaver saver;
+
+		protected override void OnCreate (Bundle savedInstanceState)
 		{
-			base.OnCreate (bundle);
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.MainLayout);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			// Create the picker instance
+			saver = Saver.CreateSaver (OneDriveAppId);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+			// Add the start saving listener
+			FindViewById (Resource.Id.startSaverButton).Click += async delegate {
+				FindViewById (Resource.Id.result_table).Visibility = ViewStates.Invisible;
+
+				var filename = FindViewById<EditText> (Resource.Id.file_name_edit_text).Text;
+				var fileSizeString = FindViewById<EditText> (Resource.Id.file_size_edit_text).Text;
+				int size;
+				if (!int.TryParse (fileSizeString, out size)) {
+					size = DefaultFileSizeKb;
+				}
+
+				// Create a file
+				var file = await CreateExternalSdCardFile (filename, size);
+
+				// Start the saver
+				saver.StartSaving (this, filename, Android.Net.Uri.Parse ("file://" + file));
 			};
+		}
+
+		protected override void OnActivityResult (int requestCode, Result resultCode, Intent data)
+		{
+			// Check that we were able to save the file on OneDrive
+			TextView overallResult = FindViewById<TextView> (Resource.Id.overall_result);
+			TextView errorResult = FindViewById<TextView> (Resource.Id.error_type_result);
+			TextView debugErrorResult = FindViewById<TextView> (Resource.Id.debug_error_result);
+
+			try {
+				saver.HandleSave (requestCode, resultCode, data);
+
+				overallResult.Text = GetString (Resource.String.overall_result_success);
+				errorResult.Text = GetString (Resource.String.error_message_none);
+				debugErrorResult.Text = GetString (Resource.String.error_message_none);
+			} catch (SaverException e) {
+				overallResult.Text = GetString (Resource.String.overall_result_failure);
+				errorResult.Text = e.ErrorType.ToString ();
+				debugErrorResult.Text = e.DebugErrorInfo;
+			}
+			FindViewById (Resource.Id.result_table).Visibility = ViewStates.Visible;
+		}
+
+		/// <summary>
+		/// Creates an file on the SDCard </summary>
+		/// <param name="filename"> The name of the file to create </param>
+		/// <param name="size"> The size in KB to make the file </param>
+		/// <returns> The <seealso cref="File"/> object that was created </returns>
+		private async Task<string> CreateExternalSdCardFile (string filename, int size)
+		{
+			const int bufferSize = 1024;
+			var random = new Random ();
+
+			// get the path
+			var fullPath = Android.OS.Environment.ExternalStorageDirectory.AbsolutePath;
+			fullPath = Path.Combine (fullPath, filename);
+
+			// create the file
+			try {
+				using (var stream = File.OpenWrite (fullPath)) {
+					// Create a 1 kb size buffer to use in writing the temp file
+					var buffer = new byte[bufferSize];
+					random.NextBytes (buffer);
+
+					// Write out the file, 1 kb at a time
+					for (int i = 0; i < size; i++) {
+						await stream.WriteAsync (buffer, 0, buffer.Length);
+					}
+				}
+			} catch (IOException ex) {
+				Toast.MakeText (this, "Error when creating the file: " + ex.Message, ToastLength.Long).Show ();
+			}
+
+			return fullPath;
 		}
 	}
 }
-
-
